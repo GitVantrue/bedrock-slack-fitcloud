afafea1de47625eb964fd0514dadb64715d25ba3
Gitventure <kimhs3194@gmail.com>
2025-07-09 22:38:28 +0900

    슈퍼바이저

--- supervisor/fitcloudSuperVisor.py
+++ supervisor/fitcloudSuperVisor.py
@@ -11,8 +11,8 @@ AGENT2_ID = os.environ.get("AGENT2_ID", "7HPRF6E9UD")
 AGENT2_KEYWORDS = ["보고서", "리포트", "엑셀", "차트", "그래프", "PDF", "파일", "첨부", "다운로드", "업로드", "슬랙"]
 
 def lambda_handler(event, context):
-    # event가 list로 들어오는 경우 방어
-    if isinstance(event, list):
+    # event가 dict가 될 때까지 반복해서 언패킹
+    while isinstance(event, list):
         event = event[0]
     try:
         user_input = event.get("parameters", {}).get("user_input")
